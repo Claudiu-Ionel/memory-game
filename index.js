aa22792fb4a2084abcc3716f37bbaa16741810ec
Claudiu <ionel.claudiu394@gmail.com>
2022-09-25 15:19:40 +0200

    fixed transform execution

--- index.js
+++ index.js
@@ -5,43 +5,48 @@ let cardsInArray = []
 
 
 cards.forEach((card) => {
-    card.addEventListener("click", () => {
-        flipCard(card)
-        cardsInArray.push({"cardElement": card, cardText: `${card.firstElementChild.nextElementSibling.innerHTML}`})
-     
-        if (containsDuplicates(cardsInArray)) {
-            cardsInArray = []
-        } 
-        if (!containsDuplicates(cardsInArray)) {
-                cardsInArray.forEach((card) => {
-                    unFlipCard(card.cardElement)
-                    cardsInArray = []
-                })
-        }
-        
-    })
+  card.addEventListener("click", () => {
+    flipCard(card)
+    cardsInArray.push({ "cardElement": card, cardText: `${card.firstElementChild.nextElementSibling.innerHTML}` })
+
+    setTimeout(() => {
+      if (containsDuplicates(cardsInArray)) {
+        cardsInArray = []
+      }
+      if (!containsDuplicates(cardsInArray)) {
+        cardsInArray.forEach((card) => {
+          unFlipCard(card.cardElement)
+          cardsInArray = []
+        })
+      }
+    }, 700);
+
+  })
 })
 
 function unflipCards() {
-    cardsArr?.filter((card)=> {
-        card.classList.toggle("flipped")
-    })
+  cardsArr?.filter((card) => {
+    card.classList.toggle("flipped")
+  })
 }
-function flipCard(cardToFlip) {
-    cardToFlip.classList.add("flipped")
+async function flipCard(cardToFlip) {
+  await cardToFlip.classList.add("flipped")
 }
 function unFlipCard(card) {
-    card.classList.remove("flipped")
+  card.classList.remove("flipped")
 }
 function containsDuplicates(array) {
-    console.log(array);
-    if (array.length >= 1){
-        if (array[0].cardText === array[1].cardText) {
-            return true;
-          }
-          return false;
+  console.log(array);
+  if (array.length >= 1) {
+
+    if (array[0].cardText === array[1].cardText) {
+      return true;
     }
+
+
   }
+
+}
 
 
 // function checkValueOfFlippedCard() {
