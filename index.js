337f208c66346292ded4eb847a6286c70596f5cb
Claudiu <ionel.claudiu394@gmail.com>
2022-09-25 15:56:29 +0200

    click cancelation when a card is flipped

--- index.js
+++ index.js
@@ -3,37 +3,56 @@ const cards = document.querySelectorAll(".flip-card")
 const cardsArr = Array.from(cards)
 let cardsInArray = []
 
+const flipCalculations = async (card) => {
+  if (!card.classList.contains("flipped")) {
+    flipCard(card).then(() => {
+      cardsInArray.push({ "cardElement": card, cardText: `${card.firstElementChild.nextElementSibling.innerHTML}` })
+    }).then(() => {
+      test()
+    })
+
+
+  }
+
+
+
+
+  // setTimeout(() => {
+  //   if (containsDuplicates(cardsInArray)) {
+  //     cardsInArray = []
+  //   }
+  //   if (!containsDuplicates(cardsInArray)) {
+  //     cardsInArray.forEach((card) => {
+  //       unFlipCard(card.cardElement)
+  //       cardsInArray = []
+  //     })
+  //   }
+  // }, 700);
+}
+
 
 cards.forEach((card) => {
-  card.addEventListener("click", () => {
-    flipCard(card)
-    cardsInArray.push({ "cardElement": card, cardText: `${card.firstElementChild.nextElementSibling.innerHTML}` })
-
-    setTimeout(() => {
-      if (containsDuplicates(cardsInArray)) {
-        cardsInArray = []
-      }
-      if (!containsDuplicates(cardsInArray)) {
-        cardsInArray.forEach((card) => {
-          unFlipCard(card.cardElement)
-          cardsInArray = []
-        })
-      }
-    }, 700);
-
-  })
+  card.addEventListener("click", () => flipCalculations(card), true)
 })
 
-function unflipCards() {
-  cardsArr?.filter((card) => {
-    card.classList.toggle("flipped")
-  })
-}
 async function flipCard(cardToFlip) {
   await cardToFlip.classList.add("flipped")
 }
 function unFlipCard(card) {
   card.classList.remove("flipped")
+}
+function test(card) {
+  setTimeout(() => {
+    if (containsDuplicates(cardsInArray)) {
+      cardsInArray = []
+    }
+    if (!containsDuplicates(cardsInArray)) {
+      cardsInArray.forEach((card) => {
+        unFlipCard(card.cardElement)
+        cardsInArray = []
+      })
+    }
+  }, 1000);
 }
 function containsDuplicates(array) {
   console.log(array);
@@ -47,15 +66,3 @@ function containsDuplicates(array) {
   }
 
 }
-
-
-// function checkValueOfFlippedCard() {
-//     cardsArr.for
-// }
-
-
-
-
-// .flip-card-container:hover .flip-card {
-//     transform: rotateY(180deg); /* <=>  rotateY(.5turn) */
-//   }
